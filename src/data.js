aacfbd4dff3963c872a20a474faec9d51034ec20
Sambit Kumar Sutar <sambitsutar1999@gmail.com>
2024-06-28 21:54:18 +0530

    done

---
+++ src/data.js
@@ -0,0 +1,12 @@
+// export by name
+
+export const desc = 'React js is used to create awesome UI designs'
+
+export const headstyle = {
+    color: "red",
+    fontSize: "60px"
+  }
+
+// export by default
+const tech = "React js"
+export default tech
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import { desc } from './data';
+import tech from './data';
+import { headstyle } from './data';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// elements --> tag----> root
+// React js: avoid using var use only const and var.
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <h1 align="center" className='heading' style={headstyle}>{tech} is super easy</h1>
+    <p>{desc}</p>
+  </>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
